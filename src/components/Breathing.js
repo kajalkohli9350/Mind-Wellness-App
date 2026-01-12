f9d9ad2a1fde45787084845e6faf8249487c7bfc
kajal kohli <kohlikajal00@gmail.com>
2026-01-12 18:15:26 +0530

    adding more

--- src/components/Breathing.js
+++ src/components/Breathing.js
@@ -2,13 +2,21 @@ import React from "react";
 
 function Breathing() {
   return (
-    <div className="card">
-      <h3>Breathing Exercise</h3>
-     <div className="breathing-instruction">
-       <p >Breathe In... Hold... Breathe Out...</p>
-     <img src="https://png.pngtree.com/background/20210711/original/pngtree-yellow-flat-circle-background-promotion-main-map-picture-image_1093101.jpg" alt="Breathing Exercise" />
-     </div>
-      <button className="exercisebutton">Start Exercise</button>
+    <div className="card3">
+      <div className="card3h3">
+        <h3>Breathing Exercise</h3>
+      </div>
+      <div className="breathing-instruction">
+        <p>Breathe In... Hold... Breathe Out...</p>
+        <img
+          src="https://png.pngtree.com/background/20210711/original/pngtree-yellow-flat-circle-background-promotion-main-map-picture-image_1093101.jpg"
+          alt="Breathing Exercise"
+        />
+      </div>
+      <div className="exercisebutton">
+        <button>Start Exercise</button>
+        <button>Pause Exercise</button>
+      </div>
     </div>
   );
 }
--- src/components/Music.js
+++ src/components/Music.js
@@ -3,10 +3,18 @@ import React from "react";
 function Music() {
   return (
     <div className="card4">
-      <div className="card4h3"><h3>Relaxing Music</h3></div>
-      <div className="music-container">
-        <source src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" type="audio/mpeg" />
-    </div>
+      <div className="card4h3">
+        <h3>Relaxing Music</h3>
+      </div>
+      <div className="player">
+        <h2>Song 1</h2>
+      </div>
+
+      <div className="controls">
+        <button >▶ </button>
+        <button>⏸</button>
+        <button >⏭</button>
+      </div>
     </div>
   );
 }
